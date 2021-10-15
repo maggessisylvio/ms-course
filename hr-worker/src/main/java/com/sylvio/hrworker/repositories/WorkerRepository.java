cdc3cbb8f359e5cdfee3a702983ecc2b7ab23c5c
Sylvio Maggessi <sylviodev@gmail.com>
2021-10-14 22:42:20 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/sylvio/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.sylvio.hrworker.repositories;
+
+import com.sylvio.hrworker.entities.Worker;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+}
