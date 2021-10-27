eb005d9589901b68c39c0922933137710f9789f8
Sylvio Maggessi <sylviodev@gmail.com>
2021-10-27 00:20:30 -0300

    hystrix fault tolerance

--- hr-payroll/src/main/java/com/sylvio/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/sylvio/hrpayroll/resources/PaymentResource.java
@@ -1,5 +1,6 @@
 package com.sylvio.hrpayroll.resources;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.sylvio.hrpayroll.entities.Payment;
 import com.sylvio.hrpayroll.services.PaymentService;
 
@@ -17,10 +18,15 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = service.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
     }
 
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Kin Kudo", 400.0, days);
+        return ResponseEntity.ok(payment);
+    }
 }
